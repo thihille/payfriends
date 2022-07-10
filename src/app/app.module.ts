03482b26626607eb63c0eb2c4c30ed166c95aed4
Thiago Hille <thiago.hille@comexport.com.br>
2022-07-10 15:41:15 -0300

    Evoluções de módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { PaymentsComponent } from './modules/pages/payments/payments.component';
 import { AuthService } from './modules/commons/services/auth.service';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/commons/shared/shared.module';
 import { LoginComponent } from './modules/pages/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PaymentsService } from './modules/pages/payments/services/payments.service';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -18,13 +24,22 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     SharedModule,
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    MessageService,
+    PaymentsService,
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/commons/shared/shared.module.ts
+++ src/app/modules/commons/shared/shared.module.ts
@@ -1,33 +1,54 @@
 import { RouterModule } from '@angular/router';
 import { IllustratorFriendComponent } from './components/illustrator-friend/illustrator-friend.component';
+import { LoaderComponent } from './components/loader/loader.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { FeatherModule } from 'angular-feather';
-import { Eye, EyeOff } from 'angular-feather/icons';
+import { Eye, EyeOff, Edit, XCircle, Filter } from 'angular-feather/icons';
+
+/*PrimeNG modules */
+import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CalendarModule } from 'primeng/calendar';
+import { ToastModule } from 'primeng/toast';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
 
 const icons = {
   Eye,
-  EyeOff
+  EyeOff,
+  Edit,
+  XCircle,
+  Filter
 };
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    FeatherModule.pick(icons)
-  ],
-  declarations: [
-    HeaderComponent,
-    ModalComponent,
-    IllustratorFriendComponent
-  ],
+  imports: [CommonModule, RouterModule, FeatherModule.pick(icons)],
+  declarations: [HeaderComponent, ModalComponent, IllustratorFriendComponent, LoaderComponent],
   exports: [
     HeaderComponent,
     ModalComponent,
     IllustratorFriendComponent,
-    FeatherModule
+    LoaderComponent,
+    FeatherModule,
+    TableModule,
+    CheckboxModule,
+    DialogModule,
+    ButtonModule,
+    InputTextModule,
+    InputNumberModule,
+    CalendarModule,
+    ToastModule,
+    OverlayPanelModule,
+    InputSwitchModule,
+    TriStateCheckboxModule
   ]
 })
-export class SharedModule { }
+export class SharedModule {}
